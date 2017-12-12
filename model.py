4704627f05a445590dc6e82f306f6a47696192fe
Yanjun Lin <yl3829@columbia.edu>
2017-12-12 18:39:19 -0500

    Prediction added

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+from numba.types import none
+from operations import map_output_to_input_tf
 
 class biaxial_model(object):
     def __init__(self,t_layer_sizes, n_layer_sizes, input_size=80,output_size=2,dropout=0.5):
@@ -46,7 +48,7 @@ class biaxial_model(object):
         
         # two layer LSTM with state drop out
         t_lstm_cells = [tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(hs),
-                                                      state_keep_prob=1-dropout,
+                                                      output_keep_prob=1-dropout,
                                                       variational_recurrent=True,
                                                       dtype=tf.float32) for hs in t_layer_sizes]
         t_multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(t_lstm_cells)
@@ -87,7 +89,7 @@ class biaxial_model(object):
         
         # two layer LSTM with state drop out
         n_lstm_cells = [tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(hs),
-                                                      state_keep_prob=1-dropout,
+                                                      output_keep_prob=1-dropout,
                                                       variational_recurrent=True,
                                                       dtype=tf.float32) for hs in n_layer_sizes]
         n_multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(n_lstm_cells)
@@ -100,7 +102,10 @@ class biaxial_model(object):
                                                           dtype=tf.float32,
                                                           scope='n_rnn')
                 
-        note_final = tf.reshape(tf.layers.dense(note_result,units=output_size,activation=tf.nn.sigmoid)
+        note_final = tf.reshape(tf.layers.dense(tf.reshape(note_result,(n_batch*n_time*n_note,self.n_layer_sizes[-1])),
+                                                units=output_size,
+                                                activation=tf.nn.sigmoid,
+                                                name='output_layer')
                                 ,(n_batch,n_time,n_note,output_size))
         
         active_notes = input_mat[:,1:,:,0:1]
@@ -114,9 +119,51 @@ class biaxial_model(object):
         gradients_clipped = [(tf.clip_by_value(t[0],-1,1),t[1]) for t in gradients]
         self.optimizer = trainer.apply_gradients(gradients_clipped)
         
+        
     def setup_predict(self):
         
-        self.predict_seed = tf.placeholder(dtype=tf.int8, shape=(None,None), name='predict_seed')
+        def _step_note(state,indata):
+            # state shape: [[100,50],2], indata shape: 300
+            indata = tf.concat((tf.expand_dims(indata,axis=0),state[1]),axis=-1)
+            hidden = state[0]
+            note_output, new_state = self.n_multi_rnn_cell.call(inputs=indata,state=hidden) # output shape: 50, new_state shape: 100 or 50 
+            output = tf.layers.dense(note_output,
+                                     units=2,
+                                     activation=tf.nn.sigmoid,
+                                     name='output_layer',
+                                     reuse=True)
+            return (new_state,output)
         
+        def _step_time(states,_):
+            hidden = states[0] # shape: (notes*t_hidden)*2 e.g. (78*300)*2
+            indata = states[1] # shape: notes*note_features e.g. 78*80
+            time = states[2]
+            
+            output, new_state = self.t_multi_rnn_cell.call(inputs=indata,state=hidden) # output shape: notes*t_hidden
+            
+            start_note_values = tf.zeros((1,2))
+            
+            n_initializer = (self.n_multi_rnn_cell.zero_state(1, tf.float32),
+                             start_note_values)
+            
+            note_result = tf.scan(_step_note,elems=output,initializer=n_initializer) # note_result shape: 78*2
+            next_input = map_output_to_input_tf(note_result[1], time)
+            next_input = tf.reshape(next_input,(-1,80))
+            time = time + 1
+            return(new_state,next_input,time) 
         
-biaxial_model([300,300],[100,50])
+        self.predict_seed = tf.placeholder(dtype=tf.float32, shape=(None,80), name='predict_seed')
+        self.step_to_sumulate = tf.placeholder(dtype=tf.int32, shape=(1))
+        
+        num_notes = tf.shape(self.predict_seed)[0]
+        
+        initializer = (self.t_multi_rnn_cell.zero_state(num_notes, tf.float32), # initial state
+                       self.predict_seed, # initial input
+                       tf.constant(0)) # time
+        
+        elems = tf.zeros(shape=self.step_to_sumulate)
+        
+        time_result = tf.scan(_step_time,elems=elems,initializer=initializer)
+        
+        self.new_song = time_result[1]
+        
