e79dd528886b87ef20e9a3859a6548bfd0485d9c
Yanjun Lin <yl3829@columbia.edu>
2017-12-18 03:51:01 -0500

    predict only

---
+++ predict.py
@@ -0,0 +1,14 @@
+import model
+import data
+
+if __name__ == '__main__':
+    
+    # load midi files as statematrix, if no file will download from "http://www.piano-midi.de"
+    pieces = data.getpices(composer='bach') 
+    
+    # Building model
+    print('Building model')
+    music_model = model.biaxial_model(t_layer_sizes=[300,300], n_layer_sizes=[100,50])
+    
+    print('Start predicting')
+    music_model.predict(pieces,'biaxial_rnn_1513531465',step=320,conservativity=1,n=20)
