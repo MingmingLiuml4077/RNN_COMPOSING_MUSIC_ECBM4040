2835f0df587f559c14e9f094472f15a8fa1a23d7
Yanjun Lin <33325520+yl3829@users.noreply.github.com>
2017-12-18 04:31:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     
     # Building model
     print('Building model')
-    music_model = model.biaxial_model(t_layer_sizes=[300,300], n_layer_sizes=[100,50])
+    music_model = model.biaxial_model(t_layer_sizes=[300,300], n_layer_sizes=[100,50],trainer = tf.train.AdamOptimizer())
     
     print('Start training')
-    music_model.train(pieces,batch_size=5,max_epoch=10000,predict_freq=100)
+    music_model.train(pieces,batch_size=10,max_epoch=10000,predict_freq=200)
